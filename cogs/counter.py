41c66369539370344e0e513cd401cc921d7a9340
NinjaCheetah <58050615+NinjaCheetah@users.noreply.github.com>
2022-05-04 19:24:56 -0400

    Added license header to every python file

--- cogs/counter.py
+++ cogs/counter.py
@@ -1,4 +1,18 @@
-# counter.py
+# Ah Counter "counter.py"
+# Copyright (C) 2022  NinjaCheetah
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 from discord.ext import commands
 import json
 import re
@@ -48,6 +62,9 @@ def has_sleep(string):
     return match
 
 class Message_Counter(commands.Cog):
+    """
+    The code that handles coutning words and saving them.
+    """
     def __init__(self, client):
         self.client = client
 
--- cogs/management.py
+++ cogs/management.py
@@ -1,3 +1,18 @@
+# Ah Counter "management.py"
+# Copyright (C) 2022  NinjaCheetah
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 import discord
 from discord.ext import commands
 import discord.utils
@@ -15,6 +30,9 @@ def save_devcounters(devcounters):
        json.dump(devcounters, f, indent=4)
 
 class Management(commands.Cog):
+    """
+    Management commands, such as setting the bot's status.
+    """
     def __init__(self, bot):
         self.bot = bot
 
